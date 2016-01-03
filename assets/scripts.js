7b59065313bc09ee747db8bea996beb94cefb79f
Andrew Gomez <gomezandrewj@gmail.com>
2016-01-03 11:52:38 -0800

    finish scrolling animation

--- assets/scripts.js
+++ assets/scripts.js
@@ -16,14 +16,6 @@
 
 			var scrollPos = $(window).scrollTop() + $(window).height() / 2;
 
-			if (scrollPos > $(window).height() * 0.7) {
-				$title.addClass('fixed-1');
-				$header.addClass('fixed-2');
-			} else {
-				$title.removeClass('fixed-1');
-				$header.removeClass('fixed-2');
-			}
-
 			$divs.each(function() {
 
 				var thisTop = $(this).offset().top;
@@ -34,17 +26,32 @@
 					$header.text(this.className.replace('-',' ').toUpperCase());
 
 					$(this).siblings().css('margin-left', '');
-					$(this).siblings().css('margin-top', 0);
+					$(this).siblings().css('margin-top', '');
+					$(this).siblings().css('margin-bottom', '');
 					$(this).siblings().removeClass('fixed-3');
 
 					$(this).css('margin-left', 0);
 					$(this).css('margin-top', '');
 					$(this).addClass('fixed-3');
 
-					$(this).next().css('margin-top', $(this).height());
+					$(this).prev('div').css('margin-bottom', $(this).height());
+
+					if ($(this).next().length) {
+						$(this).next().css('margin-top', $(this).height());
+					} 
+					
 				}
 
 			});
+
+			if (scrollPos > $(window).height() * 0.7) {
+				$title.addClass('fixed-1');
+				$header.addClass('fixed-2');
+			} else {
+				$title.removeClass('fixed-1');
+				$header.removeClass('fixed-2');
+				$divs.removeClass('fixed-3');
+			}
 
 		});
 
