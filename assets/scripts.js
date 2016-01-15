cfbf589a82ae2bcf4c27ed942d92723dab390287
Andrew Gomez <gomezandrewj@gmail.com>
2016-01-14 20:03:53 -0800

    add 2 columns to ul li's

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,12 +15,20 @@
 
 			var scrollPos = $(window).scrollTop() + $(window).height() / 2;
 
+			if (scrollPos > $(window).height() * 0.8) {
+				$header.addClass('fixed-1');
+			} else {
+				$header.removeClass('fixed-1');
+				$divs.removeClass('fixed-2');
+			}
+
 			$divs.each(function() {
 
 				var thisTop = $(this).offset().top;
 				var thisBottom = $(this).offset().top + $(this).height();
 
-				if (scrollPos >= thisTop && scrollPos <= thisBottom && this.className.indexOf('fixed-2') === -1) {
+				if (scrollPos > $(window).height() * 0.8 && scrollPos >= thisTop && 
+					scrollPos <= thisBottom && this.className.indexOf('fixed-2') === -1) {
 					
 					$header.text(this.className.replace('-',' ').toUpperCase());
 
@@ -31,21 +39,27 @@
 
 					$(this).css('margin-left', 0);
 					$(this).css('margin-top', '');
+					$(this).css('margin-bottom', '');
 					$(this).addClass('fixed-2');
 					$(this).children('ul').css('margin-left', '5%');
+					$(this).children('ul').children('li').css('margin-left', '5%');
 
-					$(this).prev('div').css('margin-bottom', $(this).height());
+					if ($(this).prev('div').length) {
+						$(this).prev('div').css('margin-bottom', $(this).height());
+					} else {
+						$(this).next('div').css('margin-top', $(this).height());
+						$(this).next('div').next('div').css('margin-top', $(this).next('div').height());
+						return false;
+					}
 					
 				}
 
-			});
+				if ($(this).next('div').length === 0 && 
+					scrollPos > $(document).height() - $(window).height() / 2) {
+					$(this).removeClass('fixed-2');
+				}
 
-			if (scrollPos > $(window).height() * 0.7) {
-				$header.addClass('fixed-1');
-			} else {
-				$header.removeClass('fixed-1');
-				$divs.removeClass('fixed-2');
-			}
+			});
 
 		});
 
--- portfolio.php
+++ portfolio.php
@@ -19,13 +19,12 @@ define( 'ANDREWDEVPORTFOLIO_MINIMUM_WP_VERSION', '3.4' );
 define( 'ANDREWDEVPORTFOLIO_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'ANDREWDEVPORTFOLIO_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
-add_action('init', 'my_init');
 add_action("template_redirect", 'my_theme_redirect');
-
+add_shortcode('andrew-contact-form', 'my_contact_form');
 
 function my_init() {
-	wp_enqueue_style('andrew-dev-portfolio', ANDREWDEVPORTFOLIO_PLUGIN_URL . 'style.min.css');
-	wp_enqueue_script('andrew-dev-portfolio', ANDREWDEVPORTFOLIO_PLUGIN_URL . 'scripts.min.js', array('jquery'));
+    wp_enqueue_style('andrew-dev-portfolio', ANDREWDEVPORTFOLIO_PLUGIN_URL . 'style.min.css');
+    wp_enqueue_script('andrew-dev-portfolio', ANDREWDEVPORTFOLIO_PLUGIN_URL . 'scripts.min.js', array('jquery'));
 }
 
 function my_theme_redirect() {
@@ -33,20 +32,42 @@ function my_theme_redirect() {
 	$plugindir = dirname( __FILE__ );
 
 	if (isset($wp->query_vars["pagename"]) && $wp->query_vars["pagename"] == 'portfolio') {
+        my_init();
 		$templatefilename = 'page-portfolio.php';
 		if (file_exists(ANDREWDEVPORTFOLIO_PLUGIN_DIR . '/templates/' . $templatefilename)) {
 			$return_template = ANDREWDEVPORTFOLIO_PLUGIN_DIR . '/templates/' . $templatefilename;
-			do_theme_redirect($return_template);
+			include($return_template);
+            die();
 		}
 	}
 }
 
-function do_theme_redirect($url) {
-    global $post, $wp_query;
-    if (have_posts()) {
-        include($url);
+function my_contact_form() {
+
+    $content = "
+        <form action=" . admin_url('admin-ajax.php') . " method='POST'>
+        <input name='action' value='email_send' type='hidden'>
+        <input name='name' type='text' placeholder='Name' />
+        <input name='email' type='text' placeholder='Email' />
+        <textarea name='message'></textarea>
+        <input type='submit' value='Send Message' />
+        </form>
+    ";
+
+    return $content;
+
+}
+
+add_action('wp_ajax_nopriv_email_send', 'process_email_send');
+
+function process_email_send() {
+
+    echo 'stuff';
+    if (empty($_POST) || !wp_verify_nonce($_POST['_wpnonce'],'email_send')) {
+        echo 'You targeted the right function, but sorry, your nonce did not verify.';
         die();
     } else {
-        $wp_query->is_404 = true;
+        // do your function here 
+        wp_redirect($redirect_url_for_non_ajax_request);
     }
 }
